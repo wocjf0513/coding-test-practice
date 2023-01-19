1a917650a24f438c8f519cac1d712d937c89ba1d
Your Name <wocjf0513@ajou.ac.kr>
2023-01-19 14:36:12 +0900

    tomato[bfs] i solved problem that i couldn't solve

--- 토마토.java
+++ 토마토.java
@@ -7,14 +7,16 @@ import java.util.StringTokenizer;
 
 public class 토마토 {
 
-    static boolean change=true;
+    
     static int[][][] box;
-
+    static int x,y,h;
     static int[] dirH={0,0,0,0,+1,-1};
     static int[] dirX={0,0,+1,-1,0,0};
     static int[] dirY={+1,-1,0,0,0,0};
 
-    static int x,y,h;
+    static long ansDay=0;
+
+    
 
     public static void main(String[] args) throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -26,7 +28,7 @@ public class 토마토 {
 
         box=new int[h+1][y+1][x+1];
 
-        
+        //입력
         for(int i=1; i<=h; i++){
             for(int j=1; j<=y; j++){        
                 st=new StringTokenizer(br.readLine());
@@ -36,86 +38,83 @@ public class 토마토 {
             }
         }
 
-        int day=-1;
-        
-    while(change){
-        change=false;
-        int[][][] check=new int[h+1][y+1][x+1];
-        int[][][] newbox=copy(box);
+        bfs();
 
-        for(int i=1; i<=h; i++){
-            for(int j=1; j<=y; j++){
-                for(int k=1; k<=x; k++)
-                {
-                    if(box[i][j][k]==1 && check[i][j][k]==0){
-                        bfs(i,j,k,check,newbox);
+        if(ansDay!=0)
+        System.out.println(ansDay);
+        
+
+    }
+        public static void bfs(){
+            Deque<int[]> q=new ArrayDeque<>();
+
+            long cnt=h*x*y;
+            boolean containsZero=false;
+            long wallCnt=0;
+
+            for(int i=1; i<=h; i++){
+                for(int j=1; j<=y; j++){        
+                    for(int k=1; k<=x; k++){
+                        if(box[i][j][k]==1){
+                            q.add(new int[]{i,j,k,0});
+                        }
+                        else if(box[i][j][k]==-1){
+                            wallCnt++;
+                        }
+                        else{
+                            containsZero=true;
+                        }
+
                     }
                 }
             }
-        }
+
         
-        box=copy(newbox);
-        day++;
-    }
 
-    if(check(box)){
-        System.out.println(day);
-    }
-    else{
-        System.out.println(-1);
-    }
+            if(!containsZero){
+                System.out.println(0);
+                return;
+            }
 
+            boolean[][][] check=new boolean[h+1][y+1][x+1];
 
-    }
-    public static void bfs(int Lh, int Ly, int Lx, int[][][] check, int[][][] newBox){
-        
-        Deque<int[]> dq=new LinkedList<>();
-        dq.add(new int[]{Lh,Ly,Lx});
-        check[Lh][Ly][Lx]=1;
+            long tomatoCnt=cnt-wallCnt;
+            long tempCnt=0;
 
-        while(!dq.isEmpty()){
-            int temp[]=dq.poll();
-            for(int i=0; i<6; i++){
-                int newH=temp[0]+dirH[i];
-                int newY=temp[1]+dirY[i];
-                int newX=temp[2]+dirX[i];
+            while(!q.isEmpty()){
+                int[] temp=q.poll();
+                tempCnt++;
+                check[temp[0]][temp[1]][temp[2]]=true;
+                int day=temp[3];
 
-                if(1<=newH && newH<=h && 1<=newY && newY<=y && 1<=newX && newX<=x ){
-                    if(box[newH][newY][newX]==0 && check[newH][newY][newX]==0){
-                        check[newH][newY][newX]=1;
-                        newBox[newH][newY][newX]=1;
-                        dq.add(new int[]{newH,newY,newX});
-                        change=true;
+                if(ansDay<day)
+                ansDay=day;
+
+                for(int i=0; i<6; i++){
+                    int lh2=temp[0]+dirH[i];
+                    int ly2=temp[1]+dirY[i];
+                    int lx2=temp[2]+dirX[i];
+
+                    if(lh2>=1 && lh2<=h && ly2>=1 && ly2<=y && lx2>=1 && lx2<=x &&!check[lh2][ly2][lx2]){
+                        if(box[lh2][ly2][lx2]==0){
+                            //익지 않은 상태                            
+                            box[lh2][ly2][lx2]=1;
+                            q.add(new int[]{lh2,ly2,lx2,day+1});
+                        }        
                     }
                 }
-            }
-            
-        }
-        
 
-    }
-    public static int[][][] copy(int[][][] source){
-        int[][][] copyList=new int[h+1][y+1][x+1];
-
-        for(int i=1; i<=h; i++){
-            for(int j=1; j<=y; j++){
-                for(int k=1; k<=x; k++)
-                copyList[i][j][k]=source[i][j][k];
             }
-        }
-        return copyList;
-    }
-    public static boolean check(int[][][] box){
-        for(int i=1; i<=h; i++){
-            for(int j=1; j<=y; j++){
-                for(int k=1; k<=x; k++){
-                if(box[i][j][k]==-1)
-                return false;
-                }
-            }
-        }
-        return true;
 
-    }
+            if(tempCnt!=tomatoCnt){
+                ansDay=0;
+                System.out.println(-1);
+                return;
+            }
+
+
+
+        }
+       
     
 }
